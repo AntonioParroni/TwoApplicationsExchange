ae52118bc992f794329bc979060cec57a5ab1070
Antonio <parroniantonio@gmail.com>
2021-03-14 13:14:24 +0200

    Test

---
+++ cmake-build-debug/CMakeFiles/TwoApplicationsExchange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TwoApplicationsExchange.dir/main.cpp.o"
+  "TwoApplicationsExchange"
+  "TwoApplicationsExchange.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TwoApplicationsExchange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
